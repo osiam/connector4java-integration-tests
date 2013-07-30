a67d78af5b1e21a108c6b12e9f531d68fa958419
Florian Wallner <wallner@balumba.org>
2013-07-30 18:28:30 +0200

    Integration tests are now self-contained using h2-database

--- src/test/java/de/osiam/client/UserServiceIT.java
+++ src/test/java/de/osiam/client/UserServiceIT.java
@@ -1,7 +1,11 @@
 package de.osiam.client;
 
+import com.github.springtestdbunit.DbUnitRule;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.osiam.client.OsiamUserService;
 import org.osiam.client.exception.NoResultException;
 import org.osiam.client.exception.UnauthorizedException;
@@ -9,31 +13,44 @@ import org.osiam.client.oauth.AccessToken;
 import org.osiam.client.oauth.AuthService;
 import org.osiam.client.oauth.GrantType;
 import org.osiam.resources.scim.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.sql.DataSource;
 import java.lang.reflect.Field;
-import java.net.URISyntaxException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
 
 import static org.junit.Assert.*;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/context.xml")
+@DatabaseSetup("/database_seed.xml")
 public class UserServiceIT {
 
-    private static final String VALID_USER_UUID = "94bbe688-4b1e-4e4e-80e7-e5ba5c4d6db4";
-    private static final String INVALID_UUID = "ffffffff-934a-4358-86bc-fffffffffff";
+    private static final String VALID_USER_UUID = "f04f1022-8870-469a-ad56-c3209aebf8eb";
+    private static final String INVALID_UUID = "ffffffff-ffff-ffff-ffff-fffffffffff";
 
     private AccessToken accessToken;
-    private UUID uuidStandardUser;
-    private String endpointAddress = "http://localhost:9090";
+    private UUID validUUID = null;
+    private String endpointAddress = "http://localhost:8080/osiam-server/";
 
     private String clientId = "example-client";
     private String clientSecret = "secret";
     private AuthService authService;
     private OsiamUserService service;
 
+    @Rule
+    public DbUnitRule dbUnit = new DbUnitRule();
+
+    @Autowired
+    DataSource dataSource;
+
     @Before
-    public void setUp() throws URISyntaxException {
+    public void setUp() throws Exception {
 
         AuthService.Builder authBuilder = new AuthService.Builder(endpointAddress).
                 withClientId(clientId).
@@ -43,21 +60,20 @@ public class UserServiceIT {
                 withPassword("koala");
         authService = authBuilder.build();
         service = new OsiamUserService.Builder(endpointAddress).build();
+        accessToken = authService.retrieveAccessToken();
     }
 
     @Test
     public void get_a_valid_user() throws Exception {
-        given_a_test_user_UUID();
-        given_a_valid_access_token();
-        User user = service.getUserByUUID(uuidStandardUser, accessToken);
-        assertEquals(VALID_USER_UUID, user.getId());
+        givenAnExistingUserWithUUID();
+        User user = service.getUserByUUID(validUUID, accessToken);
+        assertEquals(validUUID.toString(), user.getId());
     }
 
     @Test
     public void ensure_all_values_are_deserialized_correctly() throws Exception {
-        given_a_test_user_UUID();
-        given_a_valid_access_token();
-        User actualUser = service.getUserByUUID(uuidStandardUser, accessToken);
+        givenAnExistingUserWithUUID();
+        User actualUser = service.getUserByUUID(validUUID, accessToken);
 
         assertEquals("User", actualUser.getMeta().getResourceType());
         Date created = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").parse("01.08.2011 18:29:49");
@@ -102,16 +118,15 @@ public class UserServiceIT {
 
     @Test(expected = NoResultException.class)
     public void get_an_invalid_user_raises_exception() throws Exception {
-        given_a_valid_access_token();
         service.getUserByUUID(UUID.fromString("b01e0710-e9b9-4181-995f-4f1f59dc2999"), accessToken);
     }
 
     @Test(expected = UnauthorizedException.class)
     public void provide_an_invalid_access_token_raises_exception() throws Exception {
-        given_a_test_user_UUID();
+        givenAnExistingUserWithUUID();
         given_an_invalid_access_token();
 
-        service.getUserByUUID(uuidStandardUser, accessToken);
+        service.getUserByUUID(validUUID, accessToken);
         fail();
     }
 
@@ -123,14 +138,7 @@ public class UserServiceIT {
         tokenField.setAccessible(false);
     }
 
-    private void given_a_valid_access_token() throws Exception {
-        if (accessToken == null) {
-
-            accessToken = authService.retrieveAccessToken();
-        }
-    }
-
-    private void given_a_test_user_UUID() {
-        uuidStandardUser = UUID.fromString(VALID_USER_UUID);
+    private void givenAnExistingUserWithUUID() throws Exception {
+        validUUID = UUID.fromString(VALID_USER_UUID);
     }
 }
