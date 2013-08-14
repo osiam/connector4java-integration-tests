12103f78e3e0e69e4f1ccb3cdb14a31979eeddc1
Tobias Pröschold <t.proeschold@tarent.de>
2013-08-14 09:07:04 +0200

    make UserServiceIt smaller for faster Testing

--- src/test/java/de/osiam/client/UserServiceIT.java
+++ src/test/java/de/osiam/client/UserServiceIT.java
@@ -36,21 +36,12 @@ public class UserServiceIT extends AbstractIntegrationTestBase {
 
     private OsiamUserService service;
     private User deserializedUser;
-    private QueryResult<User> queryResult;
+
 
     @Before
     public void setUp() throws Exception {
         service = new OsiamUserService.Builder(endpointAddress).build();
     }
-
-    @Test
-    public void get_a_valid_user() throws Exception {
-        givenAValidUserUUID();
-        whenUserIsDeserialized();
-        assertEquals(validUUID.toString(), deserializedUser.getId());
-    }
-
-
 
     @Test
     public void name_is_deserialized_correctly() throws Exception {
@@ -81,32 +72,6 @@ public class UserServiceIT extends AbstractIntegrationTestBase {
     }
 
     @Test
-    public void photos_are_deserialized_correctly() throws Exception {
-        givenAValidUserUUID();
-        whenUserIsDeserialized();
-
-        List<MultiValuedAttribute> photos = deserializedUser.getPhotos();
-        assertEquals(1, photos.size());
-        MultiValuedAttribute photo = photos.get(0);
-
-        assertEquals("http://example.com/barbara.jpg", photo.getValue().toString());
-        assertEquals("photo", photo.getType());
-    }
-
-    @Test
-    public void ims_are_deserialized_correctly() throws Exception {
-        givenAValidUserUUID();
-        whenUserIsDeserialized();
-
-        List<MultiValuedAttribute> ims = deserializedUser.getIms();
-        assertEquals(1, ims.size());
-        MultiValuedAttribute im = ims.get(0);
-
-        assertEquals("barbara", im.getValue().toString());
-        assertEquals("xmpp", im.getType());
-    }
-
-    @Test
     public void external_id_is_deserialized_correctly() throws Exception {
         givenAValidUserUUID();
         whenUserIsDeserialized();
@@ -114,30 +79,10 @@ public class UserServiceIT extends AbstractIntegrationTestBase {
     }
 
     @Test
-    public void ensure_basic_values_are_deserialized_correctly() throws Exception {
-        givenAValidUserUUID();
-        whenUserIsDeserialized();
-
-        assertEquals("bjensen", deserializedUser.getExternalId());
-        assertEquals("BarbaraJ.", deserializedUser.getDisplayName());
-        assertEquals("de", deserializedUser.getLocale());
-        assertEquals("Barbara", deserializedUser.getNickName());
-        assertEquals("UTC", deserializedUser.getTimezone());
-        assertEquals("Dr.", deserializedUser.getTitle());
-        assertEquals("bjensen", deserializedUser.getUserName());
-        assertEquals("user", deserializedUser.getUserType());
-    }
-
-    @Test
     public void password_is_not_transferred() throws Exception {
         givenAValidUserUUID();
         whenUserIsDeserialized();
         assertEquals(null, deserializedUser.getPassword());
-    }
-
-    @Test(expected = NoResultException.class)
-    public void get_an_invalid_user_raises_exception() throws Exception {
-        service.getUserByUUID(UUID.fromString(INVALID_UUID), accessToken);
     }
 
     @Test(expected = UnauthorizedException.class)
