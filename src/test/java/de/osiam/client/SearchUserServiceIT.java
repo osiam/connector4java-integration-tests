a88eb7214181456bceed647c16ca289f1a3df7e9
David Möbius <d.moebius@tarent.de>
2013-08-15 12:42:55 +0200

    2 tests added for () and sortBy

--- src/test/java/de/osiam/client/SearchUserServiceIT.java
+++ src/test/java/de/osiam/client/SearchUserServiceIT.java
@@ -5,11 +5,13 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.osiam.client.OsiamUserService;
 import org.osiam.client.query.Query;
 import org.osiam.client.query.QueryResult;
+import org.osiam.client.query.SortOrder;
 import org.osiam.resources.scim.User;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
@@ -18,6 +20,11 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
+
+import java.awt.datatransfer.StringSelection;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/context.xml")
@@ -85,6 +92,51 @@ public class SearchUserServiceIT extends AbstractIntegrationTestBase {
         queryResultContainsUser(user01);
         queryResultContainsUser(user02);
         queryResultContainsUser(user03);
+    }
+
+    @Test
+    @Ignore //Osiam has to be fixed first. Users will only be returned if they have a email and will be
+    // returned multiple times if the hava more than one email
+    public void search_with_braces(){
+        Query.Builder innerQuery = new Query.Builder(User.class);
+        innerQuery.filter("userName").equalTo("marissa").or("userName").equalTo("hsimpson");
+
+        Query.Builder queryBuilder = new Query.Builder(User.class);
+        queryBuilder.filter("meta.created").greaterThan("2000-05-23T13:12:45.672").and(innerQuery);
+
+        queryResult = service.searchUsers(queryBuilder.build(), accessToken);
+        assertEquals(2, queryResult.getTotalResults());
+        queryResultContainsUser("marissa");
+        queryResultContainsUser("hsimpson");
+    }
+
+    @Test
+    @Ignore //the server.search method cannot work with non filter arguments at the moment
+    public void sorted_search(){
+        Query.Builder queryBuilder = new Query.Builder(User.class);
+        queryBuilder.sortBy("userName").withSortOrder(SortOrder.ASCENDING);
+        String hdfghdf = queryBuilder.build().toString();
+        queryResult = service.searchUsers(queryBuilder.build(), accessToken);
+
+        ArrayList<String> sortedUserNames = new ArrayList<>();
+        sortedUserNames.add("bjensen");
+        sortedUserNames.add("jcambell");
+        sortedUserNames.add("adavies");
+        sortedUserNames.add("cmiller");
+        sortedUserNames.add("dcooper");
+        sortedUserNames.add("epalmer");
+        sortedUserNames.add("gparker");
+        sortedUserNames.add("hsimpson");
+        sortedUserNames.add("kmorris");
+        sortedUserNames.add("ewilley");
+        sortedUserNames.add("marissa");
+        Collections.sort(sortedUserNames);
+
+        assertEquals(sortedUserNames.size(), queryResult.getTotalResults());
+        int count = 0;
+        for(User actUser : queryResult.getResources()){
+             assertEquals(sortedUserNames.get(count++), actUser.getUserName());
+        }
     }
 
     private void queryResultContainsUser(String userName) {
