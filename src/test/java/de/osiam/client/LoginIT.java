c4cd9df97efd0952bd9f4c02f3de95cc6efc0eb1
David Möbius <d.moebius@tarent.de>
2013-09-09 15:51:56 +0200

    adding tests for client_credential login flow

---
+++ src/test/java/de/osiam/client/LoginIT.java
@@ -0,0 +1,39 @@
+package de.osiam.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.osiam.client.connector.OsiamConnector;
+import org.osiam.client.oauth.AccessToken;
+import org.osiam.client.oauth.GrantType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/context.xml")
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+        DbUnitTestExecutionListener.class})
+@DatabaseSetup("/database_seed.xml")
+public class LoginIT {
+	
+    protected String endpointAddress = "http://localhost:8080/osiam-server";
+    protected String clientId = "example-client";
+    protected String clientSecret = "secret";
+    protected OsiamConnector oConnector;
+    
+	@Test
+	public void login_with_client_credentials(){
+        OsiamConnector.Builder oConBuilder = new OsiamConnector.Builder(endpointAddress).
+                setClientId(clientId).
+                setClientSecret(clientSecret).
+                setGrantType(GrantType.CLIENT_CREDENTIALS);
+        oConnector = oConBuilder.build();
+        oConnector.retrieveAccessToken();
+	}
+	
+
+}
