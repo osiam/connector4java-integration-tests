05d9d4d46bcdca4ba896c985fd6fbf51083934bf
Andreas Grau <a.grau@tarent.de>
2013-09-16 11:38:36 +0200

    Added cases for String, Enum and Boolean type to regression test for issue 2.

--- src/test/groovy/org/osiam/test/controller/ControllerIT.groovy
+++ src/test/groovy/org/osiam/test/controller/ControllerIT.groovy
@@ -54,7 +54,7 @@ class ControllerIT extends AbstractIT {
         def responseStatusCode
         def responseContentType
 
-        http.request(scope, contentType) { req ->
+        http.request(Method.GET, contentType) { req ->
             uri.path = OSIAM_ENDPOINT + requestPath
             headers."Authorization" = "Bearer " + validAccessToken.getToken()
 
@@ -79,23 +79,23 @@ class ControllerIT extends AbstractIT {
         assert responseContentType == expectedResponseType
 
         where:
-        testCase   | scope       | contentType        | expectedResponseCode | expectedResponseType             | requestPath
-        "a"        | Method.GET  | ContentType.JSON   | 200                  | "application/json;charset=UTF-8" | "/Users"
-        "b"        | Method.GET  | ContentType.JSON   | 200                  | "application/json;charset=UTF-8" | "/Users/"
-        "c"        | Method.GET  | ContentType.JSON   | 200                  | "application/json;charset=UTF-8" | "/Groups"
-        "d"        | Method.GET  | ContentType.JSON   | 200                  | "application/json;charset=UTF-8" | "/Groups/"
-        "e"        | Method.GET  | ContentType.ANY    | 200                  | "application/json;charset=UTF-8" | "/Users"
-        "f"        | Method.GET  | ContentType.TEXT   | 406                  | null                             | "/Users"
-        "g"        | Method.GET  | ContentType.BINARY | 406                  | null                             | "/Users"
-        "h"        | Method.GET  | ContentType.HTML   | 406                  | null                             | "/Users"
-        "i"        | Method.GET  | ContentType.URLENC | 406                  | null                             | "/Users"
-        "j"        | Method.GET  | ContentType.XML    | 406                  | null                             | "/Users"
-        "k"        | Method.GET  | "invalid"          | 406                  | null                             | "/Users"
-        "l"        | Method.GET  | "/"                | 406                  | null                             | "/Users"
+        testCase | requestPath  | contentType        | expectedResponseCode | expectedResponseType
+        "a"      | "/Users"     | ContentType.JSON   | 200                  | "application/json;charset=UTF-8"
+        "b"      | "/Users/"    | ContentType.JSON   | 200                  | "application/json;charset=UTF-8"
+        "c"      | "/Groups"    | ContentType.JSON   | 200                  | "application/json;charset=UTF-8"
+        "d"      | "/Groups/"   | ContentType.JSON   | 200                  | "application/json;charset=UTF-8"
+        "e"      | "/Users"     | ContentType.ANY    | 200                  | "application/json;charset=UTF-8"
+        "f"      | "/Users"     | ContentType.TEXT   | 406                  | null
+        "g"      | "/Users"     | ContentType.BINARY | 406                  | null
+        "h"      | "/Users"     | ContentType.HTML   | 406                  | null
+        "i"      | "/Users"     | ContentType.URLENC | 406                  | null
+        "j"      | "/Users"     | ContentType.XML    | 406                  | null
+        "k"      | "/Users"     | "invalid"          | 406                  | null
+        "l"      | "/Users"     | "/"                | 406                  | null
     }
 
-    //@Unroll
-    def "REGT-002-#testCase: A search operation on path #requestPath with search string #searchString should work."() {
+    @Unroll
+    def "REGT-002-#testCase: A search operation on path #requestPath with search string #searchString should return HTTP status code #expectedResponseCode."() {
         given: "a valid access token"
         AccessToken validAccessToken = osiamConnector.retrieveAccessToken()
 
@@ -104,9 +104,10 @@ class ControllerIT extends AbstractIT {
 
         def responseStatusCode
         def responseContentType
+        def responseFailureText
 
         http.request(Method.GET, ContentType.JSON) { req ->
-            uri.path = OSIAM_ENDPOINT + requestPath
+            uri.path = OSIAM_ENDPOINT + "/Users"
             uri.query = [ filter:searchString ]
             headers."Authorization" = "Bearer " + validAccessToken.getToken()
 
@@ -117,7 +118,8 @@ class ControllerIT extends AbstractIT {
             }
 
             // handler for any failure status code:
-            response.failure = { resp ->
+            response.failure = { resp, json ->
+                responseFailureText = json
                 responseStatusCode = resp.statusLine.statusCode
                 contentType = resp.headers."Content-Type"
             }
@@ -127,12 +129,34 @@ class ControllerIT extends AbstractIT {
         then: "the response should be as expected"
         assert responseStatusCode == expectedResponseCode
 
-        expect: "the response type should be as expected"
-        assert responseContentType == expectedResponseType
+        expect: "the response text should be as expected"
+        assert responseFailureText == expectedResponseFailureText
 
         where:
-        testCase   | scope       | contentType        | expectedResponseCode | expectedResponseType             | requestPath | searchString
-        "a"        | Method.GET  | ContentType.JSON   | 200                  | "application/json;charset=UTF-8" | "/Users"    | "emails.type co work"
-        "b"        | Method.GET  | ContentType.JSON   | 200                  | "application/json;charset=UTF-8" | "/Users"    | "userName co mar"
+        testCase | searchString          | expectedResponseCode | expectedResponseFailureText
+        "a"      | "userName eq marissa" | 200                  | null                                                                                              // String
+        "b"      | "userName co ari"     | 200                  | null                                                                                              // String
+        "c"      | "userName sw mar"     | 200                  | null                                                                                              // String
+        "d"      | "userName pr"         | 200                  | null                                                                                              // String
+        "e"      | "userName gt l"       | 200                  | null                                                                                              // String
+        "f"      | "userName ge m"       | 200                  | null                                                                                              // String
+        "g"      | "userName lt n"       | 200                  | null                                                                                              // String
+        "h"      | "userName le m"       | 200                  | null                                                                                              // String
+        "i"      | "emails.type eq work" | 200                  | null                                                                                              // Enum
+        "j"      | "emails.type co work" | 409                  | "[error_code:CONFLICT, description:String filter operator is not applicable on Enum type.]"       // Enum
+        "k"      | "emails.type sw rk"   | 409                  | "[error_code:CONFLICT, description:String filter operator is not applicable on Enum type.]"       // Enum
+        "l"      | "emails.type pr work" | 200                  | null                                                                                              // Enum
+        "m"      | "emails.type gt work" | 200                  | null                                                                                              // Enum
+        "n"      | "emails.type ge work" | 200                  | null                                                                                              // Enum
+        "o"      | "emails.type lt work" | 200                  | null                                                                                              // Enum
+        "p"      | "emails.type le work" | 200                  | null                                                                                              // Enum
+        "q"      | "active eq true"      | 200                  | null                                                                                              // boolean
+        "r"      | "active co true"      | 409                  | "[error_code:CONFLICT, description:String filter operator is not applicable on Boolean type.]"    // boolean
+        "s"      | "active sw true"      | 409                  | "[error_code:CONFLICT, description:String filter operator is not applicable on Boolean type.]"    // boolean
+        "t"      | "active pr true"      | 200                  | null                                                                                              // boolean
+        "u"      | "active gt true"      | 200                  | null                                                                                              // boolean
+        "v"      | "active ge true"      | 200                  | null                                                                                              // boolean
+        "w"      | "active lt true"      | 200                  | null                                                                                              // boolean
+        "x"      | "active le true"      | 200                  | null                                                                                              // boolean
     }
 }
