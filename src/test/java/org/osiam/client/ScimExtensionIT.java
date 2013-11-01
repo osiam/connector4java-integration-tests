b422bd85ea8821a8976894892aacb679ca3c0a23
Florian Wallner <wallner@balumba.org>
2013-11-01 12:31:34 +0100

    Implemented integration test for post and put of users with extensions

--- src/test/java/org/osiam/client/ScimExtensionIT.java
+++ src/test/java/org/osiam/client/ScimExtensionIT.java
@@ -1,12 +1,12 @@
 package org.osiam.client;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseOperation;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.DatabaseTearDown;
 import com.github.springtestdbunit.annotation.ExpectedDatabase;
 import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.osiam.resources.scim.Extension;
@@ -16,28 +16,38 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/context.xml")
-@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class })
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class})
 @DatabaseSetup(value = "/database_seed_extensions.xml")
-//@DatabaseTearDown(value = "/database_seed_extensions.xml", type = DatabaseOperation.DELETE_ALL)
+@DatabaseTearDown(value = "/database_seed_extensions.xml", type = DatabaseOperation.DELETE_ALL)
 public class ScimExtensionIT extends AbstractIntegrationTestBase {
 
     private static final String URN = "extension";
+    private static final String EXISTING_USER_UUID = "df7d06b2-b6ee-42b1-8c1b-4bd1176cc8d4";
 
-    @Test
-  //  @ExpectedDatabase(value = "/database_expected_extensions.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
-    public void adding_a_user_with_extension_data_to_database_works() {
-        // given
-        Map<String,String> extensionData = new HashMap<>();
+    private Map<String, String> extensionData;
+
+    @Before
+    public void setUp() {
+        extensionData = new HashMap<>();
         extensionData.put("gender", "male");
         extensionData.put("size", "1337");
         extensionData.put("birth", "Wed Oct 30 16:54:00 CET 1985");
         extensionData.put("newsletter", "false");
         extensionData.put("married", "false");
+    }
+
+    @Test
+    @ExpectedDatabase(value = "/database_expected_extensions.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+    public void adding_a_user_with_extension_data_to_database_works() {
+        // given
+
         Extension extension = new Extension(URN, extensionData);
         User user = new User.Builder("userName")
                 .setPassword("password")
@@ -45,24 +55,32 @@ public class ScimExtensionIT extends AbstractIntegrationTestBase {
                 .build();
 
         //when:
-        User userCreated = oConnector.createUser(user, accessToken);
+        String uuid = oConnector.createUser(user, accessToken).getId();
 
         //then
-        userCreated.getUserName().equals("userName");
+        User storedUser = oConnector.getUser(uuid, accessToken);
+        Extension storedExtension = storedUser.getExtension(URN);
+        for (Map.Entry<String, String> entry : extensionData.entrySet()) {
+            assertEquals(entry.getValue(), storedExtension.getField(entry.getKey()));
+        }
     }
 
     @Test
-    //  @ExpectedDatabase(value = "/database_expected_extensions.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
     public void updating_a_user_with_extension_data_to_database_works() {
         // given
-        User existingUser = oConnector.getUser("df7d06b2-b6ee-42b1-8c1b-4bd1176cc8d4",accessToken);
+        User existingUser = oConnector.getUser(EXISTING_USER_UUID, accessToken);
 
+        extensionData.put("gender", "female");
         Extension extension = existingUser.getExtension(URN);
         extension.setField("gender", "female");
 
         oConnector.updateUser(existingUser, accessToken);
 
-
+        User storedUser = oConnector.getUser(EXISTING_USER_UUID, accessToken);
+        Extension storedExtension = storedUser.getExtension(URN);
+        for (Map.Entry<String, String> entry : extensionData.entrySet()) {
+            assertEquals(entry.getValue(), storedExtension.getField(entry.getKey()));
+        }
 
 
     }
