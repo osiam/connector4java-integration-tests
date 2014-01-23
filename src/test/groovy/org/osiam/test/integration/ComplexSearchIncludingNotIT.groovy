a0e6d3e9227f6e25ec0f8da0b3c0748dfbd4acd7
David Crome <d.crome@tarent.de>
2014-01-23 16:22:30 +0100

    [test] adjusted test because of fix for BT-32

--- src/test/groovy/org/osiam/test/integration/ComplexSearchIncludingNotIT.groovy
+++ src/test/groovy/org/osiam/test/integration/ComplexSearchIncludingNotIT.groovy
@@ -8,41 +8,40 @@ import org.osiam.resources.scim.User
 
 /**
  * Some complex search criteria including the not operator.
- * User: Jochen Todea
- * Date: 12.12.13
- * Time: 16:36
- * Created: with Intellij IDEA
+ * @author Jochen Todea
  */
 class ComplexSearchIncludingNotIT extends AbstractIT {
 
     def setup() {
-        setupDatabase("database_seed_parser_with_not.xml")
+        setupDatabase('database_seed_parser_with_not.xml')
     }
 
-    def "search with not operator"() {
+    def 'search with not operator'() {
         given:
-        Query.Filter notFilter2 = new Query.Filter(User).not(new Comparison("groups.display eq \"alumni\""))
-        Query.Filter notFilter3 = new Query.Filter(User).not(new Comparison("groups.display eq \"noob\""))
+        Query.Filter notFilter2 = new Query.Filter(User).not(new Comparison('groups.display eq \"alumni\"'))
+        Query.Filter notFilter3 = new Query.Filter(User).not(new Comparison('groups.display eq \"noob\"'))
 
-        Query.Filter notFilter = new Query.Filter(User).not(new Comparison("groups.display eq \"student\"")).and(notFilter2).and(notFilter3)
-        Query.Filter filter = new Query.Filter(User, notFilter).and(new Comparison("active eq \"true\""))
+        Query.Filter notFilter = new Query.Filter(User).not(new Comparison('groups.display eq \"student\"')).and(notFilter2).and(notFilter3)
+        Query.Filter filter = new Query.Filter(User, notFilter).and(new Comparison('active eq \"true\"'))
         Query query = new Query.Builder(User).setFilter(filter).build()
 
         when:
         SCIMSearchResult<User> result = osiamConnector.searchUsers(query, accessToken)
 
         then:
-        result.getResources().size() == 1
-        result.getResources().each {
-            assert it.getUserName().equals("george0")
-        }
+        result.getResources().size() == 2
+        def userNames = result.getResources().collect {
+			it.userName
+		}
+		userNames.contains('george0')
+		userNames.contains('george2')
     }
 
-    def "combined search with 'and' and 'or'"() {
+    def 'combined search with "and" and "or"'() {
         given:
-        Query.Filter innerFilter = new Query.Filter(User, new Comparison("emails.value eq \"jj@tt.de\""))
-                .or(new Comparison("emails.value eq \"oo@aa.de\"")).or(new Comparison("emails.value eq \"bb@ss.de\""))
-        Query.Filter filter = new Query.Filter(User, new Comparison("groups.display eq \"alumni\"")).and(innerFilter)
+        Query.Filter innerFilter = new Query.Filter(User, new Comparison('emails.value eq \"jj@tt.de\"'))
+                .or(new Comparison('emails.value eq \"oo@aa.de\"')).or(new Comparison('emails.value eq \"bb@ss.de\"'))
+        Query.Filter filter = new Query.Filter(User, new Comparison('groups.display eq \"alumni\"')).and(innerFilter)
 
         Query query = new Query.Builder(User).setFilter(filter).build()
 
@@ -52,11 +51,11 @@ class ComplexSearchIncludingNotIT extends AbstractIT {
         then:
         result.getResources().size() == 2
         result.getResources().each {
-            assert (it.getUserName().equals("george4") || it.getUserName().equals("george5"))
+            assert (it.getUserName().equals('george4') || it.getUserName().equals('george5'))
         }
     }
 
-    def "searching with escaped quot in value"() {
+    def 'searching with escaped quot in value'() {
         given:
         Query query = new Query.Builder(User).setFilter('userName eq "george \\"alexander\\""').build()
 
@@ -66,11 +65,11 @@ class ComplexSearchIncludingNotIT extends AbstractIT {
         then:
         result.getResources().size() == 1
         result.getResources().each {
-            assert it.getUserName().equals("george \"alexander\"")
+            assert it.getUserName().equals('george \"alexander\"')
         }
     }
 
-    def "search with missing quotes should end up in a exception"(){
+    def 'search with missing quotes should end up in a exception'(){
         given:
         Query query = new Query.Builder(User).setFilter('userName eq george0').build()
 
