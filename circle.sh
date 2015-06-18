08b65901db0734ecab238d75764883560b84f84a
Thomas Krille <t.krille@tarent.de>
2015-06-18 10:49:51 +0200

    Make removeIntermediateImages configurable
    This is to work around an issue that happens with Docker 1.6.2-circleci
    and maven-docker-plugin.

--- circle.sh
+++ circle.sh
@@ -2,15 +2,15 @@
 
 case $CIRCLE_NODE_INDEX in
     0)
-        mvn verify -P postgres -Dit.test=org.osiam.test.integration.*IT
+        mvn verify -P postgres -Dit.test=org.osiam.test.integration.*IT -Ddocker.removeIntermediateImages=false
         ;;
     1)
-        mvn verify -P mysql -Dit.test=org.osiam.test.integration.*IT
+        mvn verify -P mysql -Dit.test=org.osiam.test.integration.*IT -Ddocker.removeIntermediateImages=false
         ;;
     2)
-        mvn verify -P postgres -Dit.test=org.osiam.client.*IT
+        mvn verify -P postgres -Dit.test=org.osiam.client.*IT -Ddocker.removeIntermediateImages=false
         ;;
     3)
-        mvn verify -P mysql -Dit.test=org.osiam.client.*IT
+        mvn verify -P mysql -Dit.test=org.osiam.client.*IT -Ddocker.removeIntermediateImages=false
         ;;
 esac
