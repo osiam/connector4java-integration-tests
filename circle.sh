d45a6fa761cab3fa22d121a5b2f3f709e37f6de1
David Crome <davcrome@gmail.com>
2017-02-10 22:56:57 +0100

    Use Maven wrapper

--- circle.sh
+++ circle.sh
@@ -2,15 +2,15 @@
 
 case $CIRCLE_NODE_INDEX in
     0)
-        mvn verify -P postgres -Dit.test=org.osiam.test.integration.*IT -Ddocker.removeIntermediateImages=false
+        ./mvnw verify -P postgres -Dit.test=org.osiam.test.integration.*IT -Ddocker.removeIntermediateImages=false
         ;;
     1)
-        mvn verify -P mysql -Dit.test=org.osiam.test.integration.*IT -Ddocker.removeIntermediateImages=false
+        ./mvnw verify -P mysql -Dit.test=org.osiam.test.integration.*IT -Ddocker.removeIntermediateImages=false
         ;;
     2)
-        mvn verify -P postgres -Dit.test=org.osiam.client.*IT -Ddocker.removeIntermediateImages=false
+        ./mvnw verify -P postgres -Dit.test=org.osiam.client.*IT -Ddocker.removeIntermediateImages=false
         ;;
     3)
-        mvn verify -P mysql -Dit.test=org.osiam.client.*IT -Ddocker.removeIntermediateImages=false
+        ./mvnw verify -P mysql -Dit.test=org.osiam.client.*IT -Ddocker.removeIntermediateImages=false
         ;;
 esac
